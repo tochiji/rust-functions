4ee231722c61b3a4b63b54e69ba6ee893c2c4039
tochiji <7910558+tochiji@users.noreply.github.com>
2021-10-29 03:22:06 +0900

    add branches project

---
+++ branches/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
+    } else {
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    // numberの値は、{}です
+    println!("The value of number is: {}", number);
+
+    loop {
+        println!("again!");
+        break;
+    }
+
+    let mut i = 3;
+
+    while i != 0 {
+        println!("{} !", i);
+
+        i = i - 1;
+    }
+
+    // 発射！
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..10).rev() {
+        println!("{}", number);
+    }
+}
